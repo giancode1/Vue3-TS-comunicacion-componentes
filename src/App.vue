04e15f2c47fa35fbd42cd0ffb3e28e558ddfa5f3
giancode1 <gieg5c@gmail.com>
2022-03-08 23:35:02 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import SizeSelector from './components/SizeSelector.vue';
+import { ref } from 'vue';
+
+const selectedSize = ref('m')
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <SizeSelector 
+    :selected-size="selectedSize" 
+    @on-change-size="selectedSize = $event" 
+    msg="Vue 3 + TypeScript + Vite"/>
+  <h3>Talla Seleccionada: {{selectedSize}}</h3>
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+button{
+  padding: 5px;
+  margin-right: 5px;
+  margin-top: 10px;
+  border:none;
+  border-radius: 5px;
+  width: 75px;
+  cursor: pointer;
+}
+.active{
+  background-color: black;
+  color: white;
+  transition: all .3s ease-out;
+}
+</style>
---
+++ src/components/SizeSelector.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// defineProps<{ msg: string }>()
+
+const count = ref(0)
+const Sizes = ['xs', 's', 'm', 'l', 'xl', 'xxl']
+
+//Javascript
+// defineProps({
+//   msg: String,
+//   selectedSize: { type: String, required: true }
+// })
+
+// const emits = defineEmits([
+//   // 'size-changed': String 
+//   'onChangeSize'
+// ])
+
+
+//Typescript
+// const props = defineProps({
+//   msg: { type: String },
+//   selectedSize: { type: String, required: true }
+// })
+
+//o
+const props = defineProps<{
+  msg: string,
+  selectedSize: string
+}>()
+
+const emits = defineEmits<{
+  (event: 'onChangeSize', size: string): void
+}>()
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <!-- <h3>selectedSize: {{ selectedSize }}</h3> -->
+  <div>
+    <button type="button" @click="count++">count is: {{ count }}</button>
+  </div>
+
+  <button
+    v-for="size in Sizes"
+    :class="{ 'active': selectedSize === size }"
+    @click="emits('onChangeSize', size)"
+  >{{ size }}</button>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
